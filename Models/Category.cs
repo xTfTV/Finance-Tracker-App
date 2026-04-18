47a6581599e443346c66fa65713d23e56bd1e8f5
xTfTV <mscott20003@icloud.com>
2026-04-17 23:48:00 -0400

    My first commit

---
+++ Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        // Defining the DB structure for the categories table
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName  = "nvarchar(100)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense"; // Can be expense/income but set to expense automatically
+    }
+}
